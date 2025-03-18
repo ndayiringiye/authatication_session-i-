08c0f69f76bfddad6c1a7b90eac3dd3d0eb93d12
ndayingiye <ndayiringiyedavid120@gmail.com>
2025-03-18 20:42:50 +0200

    feat:(vip) developing login and sugnup

---
+++ routes/login.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const User = require("../models/user.model.js");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
+
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (!user) return res.status(400).json({ msg: "Invalid Credentials" });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) return res.status(400).json({ msg: "Invalid Credentials" });
+
+        const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
+
+        res.json({ token, user: { id: user._id, username: user.username, email: user.email, role: user.role } });
+    } catch (error) {
+        res.status(500).json({ msg: "Server Error", error });
+    }
+});
+
+module.exports = router;
